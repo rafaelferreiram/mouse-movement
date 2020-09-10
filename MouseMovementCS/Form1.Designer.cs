183df406dcb9f404be02984daefb28c10f61512b
Cainã Tavares <tavares.caina@gmail.com>
2020-09-09 21:38:55 -0300

    C# Code

---
+++ MouseMovementCS/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MouseMovementCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.iniciarButton = new System.Windows.Forms.Button();
+            this.sairButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // iniciarButton
+            // 
+            this.iniciarButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.iniciarButton.Location = new System.Drawing.Point(12, 85);
+            this.iniciarButton.Name = "iniciarButton";
+            this.iniciarButton.Size = new System.Drawing.Size(143, 47);
+            this.iniciarButton.TabIndex = 0;
+            this.iniciarButton.Text = "Iniciar";
+            this.iniciarButton.UseVisualStyleBackColor = true;
+            this.iniciarButton.Click += new System.EventHandler(this.IniciarButton_Click);
+            // 
+            // sairButton
+            // 
+            this.sairButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.sairButton.Location = new System.Drawing.Point(200, 85);
+            this.sairButton.Name = "sairButton";
+            this.sairButton.Size = new System.Drawing.Size(143, 47);
+            this.sairButton.TabIndex = 1;
+            this.sairButton.Text = "Sair";
+            this.sairButton.UseVisualStyleBackColor = true;
+            this.sairButton.Click += new System.EventHandler(this.SairButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bem vindo! Vamos começar?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.sairButton;
+            this.ClientSize = new System.Drawing.Size(355, 146);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sairButton);
+            this.Controls.Add(this.iniciarButton);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Mouse Movement";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button iniciarButton;
+        private System.Windows.Forms.Button sairButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MouseMovementCS/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseMovementCS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SairButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void IniciarButton_Click(object sender, EventArgs e)
+        {
+            {
+                int altura = Screen.PrimaryScreen.Bounds.Height / 2 - 10;
+                int comprimento = Screen.PrimaryScreen.Bounds.Width;
+                double diametro = Math.PI * 2.0;
+
+                //Move Mouse
+                var mouseMovement = Task.Run(async () =>
+                {
+                    while (true)
+                    { 
+                        for (int x = 0; x < comprimento; x++)
+                        {
+                            int y = altura * Convert.ToInt32(Math.Sin(diametro)) * x / comprimento + altura;
+                            System.Drawing.Point mouseMent = new System.Drawing.Point(x, y);
+                            Cursor.Position = mouseMent;
+                        }
+                        await Task.Delay(1500);
+                    }
+                });
+            }
+        }
+    }
+}
